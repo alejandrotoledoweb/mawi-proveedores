0c091ea6dee49b1e064429de497a63ad60de37a8
alejandro toledo <atoledofr@gmail.com>
2021-09-30 18:47:52 -0500

    update validation to only use hours, not minutes either seconds

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,8 +3,8 @@ class Appointment < ApplicationRecord
   belongs_to :trabajo
 
   validates_date :date, on_or_after: lambda { Date.current }
-  validates :start_time, presence: true, format: {with: /[0-9]{2}:[0]{2}/}
-  validates :end_time, presence: true, format: {with: /[0-9]{2}:[0]{2}/}
+  validates :start_time, presence: true, format: {with: /[0-9]{2}:[0]{2}:[0]{2/}
+  validates :end_time, presence: true, format: {with: /[0-9]{2}:[0]{2}:[0]{2}/}
   validates_time :start_time, between: ['9:00am', '4:00pm']
   validates_time :end_time, between: ['8:00am', '5:00pm']
   validates :date, presence: true
