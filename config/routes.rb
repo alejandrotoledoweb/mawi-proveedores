624f9d835d4719c211c98bcca68b562dc012a328
alejandro toledo <atoledofr@gmail.com>
2021-10-01 17:19:56 -0500

    add route for hours method in appointments controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/proveedores/:id/espacio/:date/:hour', to: 'appointments#espacio'
   get '/proveedores/free/:date', to: 'appointments#disponibles'
   get '/appointments/proveedores/:id', to: 'appointments#allAppointments'
+  get 'proveedores/total_hours/:date', to: 'appointments#hours'
 end
