bab2248dce8b47c79642a82c8ab407a772c52a20
alejandro toledo <atoledofr@gmail.com>
2021-10-01 15:36:46 -0500

    add seed for more appointments and add the allAppointments method

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -26,17 +26,59 @@ class AppointmentsController < ApplicationController
   end
 
   def disponibles
-    @appointments = Appointment.all
     @free_proveedors = Appointment.all.filter do |p|
       p.date.to_s != params[:date]
-      
     end
+
     if @free_proveedors.any?
       render json: @free_proveedors, status: :ok
     else
       render json: {avaliable_proveedors: none}, status: :ok
     end
   end
+
+  def allAppointments
+    # @appointments = Appointment.find_by(proveedor_id: params[:id])
+    @appointments = Appointment.all.order(date: :desc)
+    @supporteById = @appointments.filter do |p|
+      p.proveedor_id = params[:id]
+    end
+    @allAppArray = []
+    @allAppointments = {}
+    @supporteById.each do |app|
+      @allAppointments[app.date] << {
+        trabajo_id: app.trabajo_id,
+        proveedor_id: app.proveedor_id,
+        date: app.date,
+        start_time: app.start_time,
+        end_time: app.end_time
+      }
+      @allAppArray << @allAppointments[app.date]
+      @allAppArray
+    end
+
+
+    
+    # new_hash = {}
+    # current_hash.each do |value_array|
+    #   new_hash[key] ||= {}
+    
+    #   # value_array.each do |values|
+    #   #   new_values = values.dup
+    #   #   new_values.delete(:id)
+    
+    #     new_values.each do |attribute_name, attribute_value|
+    #       new_hash[date] ||= []
+    #       new_hash[key][attribute_name] << attribute_value
+    #     end
+    #   end
+    # end
+
+
+      render json: @allAppointments, status: :ok
+    
+  end
+  
   
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
   get '/proveedores/:id/espacio/:date/:hour', to: 'appointments#espacio'
   get '/proveedores/free/:date', to: 'appointments#disponibles'
+  get '/appointments/proveedores/:id', to: 'appointments#allAppointments'
 end
