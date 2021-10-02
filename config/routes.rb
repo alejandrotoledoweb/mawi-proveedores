70893ce57487de87b5ba31f64316b83807f58f51
alejandro toledo <atoledofr@gmail.com>
2021-10-02 17:53:11 -0500

    add screenshots and description of the project in the ReadMe file

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   resources :appointments, only: %i[create index]
 
   get '/proveedores/:id/espacio/:date/:hour', to: 'appointments#espacio'
+
   get '/proveedores/free/:date', to: 'appointments#disponibles'
+
   get '/appointments/proveedores/:id', to: 'appointments#allAppointments'
+  
   get '/proveedores/:id/total_hours/:date', to: 'appointments#hours'
 end
